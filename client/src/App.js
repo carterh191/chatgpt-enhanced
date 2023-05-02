02f44413e2810dc351becdd0891907747350fb3a
Harrison <38048257+carterh191@users.noreply.github.com>
2023-05-02 12:53:52 -0700

    updated chatgps css styles

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,30 @@ function App() {
         </div>
       </aside>
       <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+              <div className="avatar">
+
+              </div>
+              <div className="message">
+                Hello World
+              </div>
+            </div>
+          </div>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar">
+
+              </div>
+              <div className="message">
+                I am an AI
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="chat-input-holder">
-          <textarea className="chat-input-textarea" placeholder="type your message here"></textarea>
+          <textarea rows="1" className="chat-input-textarea"></textarea>
         </div>
       </section>
       
