d1520e861744b5df5798bc70b0167bfaa2c7a2af
Harrison <38048257+carterh191@users.noreply.github.com>
2023-05-03 13:29:11 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+
 const configuration = new Configuration({
     organization: "org-Xensq0feVgx5AVs64jV0j1im",
     apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
-// const response = await openai.listEngines();
+
+// createa simple express api that calls the function above
+
+const app = express()
+const port = 3000
+
+app.post('/', async (req, res) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+      });
+      console.log(response.data.choices[0].text)
+      res.json({
+        data: response.data
+      })
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
