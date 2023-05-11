7c25d1d8ad89d68a0e17c17fca4dc13112799b8e
Harrison <38048257+carterh191@users.noreply.github.com>
2023-05-11 15:57:29 -0700

    fetch response
    error:
    Access to fetch at 'http://localhost:3080/' from origin 'http://localhost:3000' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const { Configuration, OpenAIApi } = require("openai");
-const express = require('express')
+const express = require("express");
 
 const configuration = new Configuration({
-    organization: "org-Xensq0feVgx5AVs64jV0j1im",
-    apiKey: "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",
+  organization: "org-Xensq0feVgx5AVs64jV0j1im",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
 // createa simple express api that calls the function above
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3080;
 
-app.post('/', async (req, res) => {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
-      });
-      console.log(response.data.choices[0].text)
-      res.json({
-        data: response.data
-      })
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  console.log(message);
+  // const response = await openai.createCompletion({
+  //   model: "text-davinci-003",
+  //   prompt: "Say this is a test",
+  //   max_tokens: 7,
+  //   temperature: 0,
+  // });
+  // console.log(response.data.choices[0].text);
+  res.json({
+    // data: response.data,
+    data: message,
+  });
 });
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
